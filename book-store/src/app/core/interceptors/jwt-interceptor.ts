a1ecefb0cb04c6e8275114a2a16799b1acf8a63a
CallMeKhiloo <khm40005000@outlook.com>
2026-02-25 17:26:16 +0200

    fix: validate if the token is expired

--- book-store/src/app/core/interceptors/jwt-interceptor.ts
+++ book-store/src/app/core/interceptors/jwt-interceptor.ts
@@ -1,7 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(Auth);
+  const router = inject(Router);
   const token = localStorage.getItem('token');
+
   if (token) {
     req = req.clone({
       setHeaders: {
@@ -10,5 +17,13 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    }),
+  );
 };
